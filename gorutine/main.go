46b2f14694a5a26b98784d8ee482fbf99bade0bd
kumackey <kumak1t09e0@gmail.com>
2021-10-07 00:45:57 +0900

    add: WaitGroupを使ったgoroutine

---
+++ gorutine/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func main() {
+	fmt.Println("what is today's lucky number?")
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		getLuckyNum()
+	}()
+
+	wg.Wait()
+}
+
+func getLuckyNum() {
+	fmt.Println("...")
+
+	// 占いにかかる時間はランダム
+	rand.Seed(time.Now().Unix())
+	time.Sleep(time.Duration(rand.Intn(3000)) * time.Millisecond)
+
+	num := rand.Intn(10)
+	fmt.Printf("Today's your lucky number is %d!\n", num)
+}
