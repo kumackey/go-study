8b40d1e456be945df661962dc5ab98ce3d313aa8
kumackey <kumak1t09e0@gmail.com>
2022-01-08 23:41:28 +0900

    再申請の作成まで

---
+++ status/application.go
@@ -0,0 +1,92 @@
+package status
+
+import (
+	"errors"
+	"time"
+)
+
+type Status string
+
+const (
+	Created   Status = "created"
+	Submitted Status = "submitted"
+	Approved  Status = "approved"
+	Rejected  Status = "rejected"
+)
+
+var ErrInvalidStatusTransition = errors.New("invalid status transition") // 不正な申請ステータス遷移
+
+type Application struct {
+	applicant         string     // 申請者
+	createdAt         *time.Time // 作成日時
+	content           string     // 申請内容
+	submittedAt       *time.Time // 提出日時
+	reviewer          string     // 申請レビューア
+	approvedAt        *time.Time // 承認日時
+	rejectedReason    string     // 否認理由
+	rejectedAt        *time.Time // 否認日時
+	countOfSubmission int        // 提出回数
+	status            Status     // 申請ステータス
+}
+
+func (a *Application) IsApplying() bool {
+	return a.status == Submitted
+}
+
+func (a *Application) IsApproved() interface{} {
+	return a.status == Approved
+}
+
+func (a *Application) IsRejected() interface{} {
+	return a.status == Rejected
+}
+
+func (a *Application) Submit(content string, submittedAt time.Time) error {
+	a.content = content
+	a.submittedAt = &submittedAt
+	a.countOfSubmission = 1
+	a.status = Submitted
+
+	return nil
+}
+
+func (a *Application) Approve(reviewer string, approvedAt time.Time) error {
+	if !a.IsApplying() {
+		return ErrInvalidStatusTransition
+	}
+
+	a.reviewer = reviewer
+	a.approvedAt = &approvedAt
+	a.status = Approved
+
+	return nil
+}
+
+func (a *Application) Reject(reviewer string, rejectedReason string, rejectedAt time.Time) error {
+	if !a.IsApplying() {
+		return ErrInvalidStatusTransition
+	}
+
+	a.reviewer = reviewer
+	a.rejectedReason = rejectedReason
+	a.rejectedAt = &rejectedAt
+	a.status = Rejected
+
+	return nil
+}
+
+func (a *Application) Resubmit(content string) error {
+	a.content = content
+	a.countOfSubmission += 1
+	a.status = Submitted
+
+	return nil
+}
+
+func NewApplication(applicant string, createdAt time.Time) *Application {
+	return &Application{
+		applicant: applicant,
+		createdAt: &createdAt,
+		status:    Created,
+	}
+}
---
+++ status/application_test.go
@@ -0,0 +1,67 @@
+package status
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func submit() *Application {
+	app := NewApplication("さとう　しんせいしゃ", time.Now())
+	_ = app.Submit("審査される何か内容", time.Now()) // 申請を提出
+
+	return app
+}
+
+func TestApplication_Submit(t *testing.T) {
+	app := NewApplication("さとう　しんせいしゃ", time.Now()) // 申請を作成
+	assert.Equal(t, false, app.IsApplying())        // 申請を作成したばかりなので申請中でない
+
+	err := app.Submit("審査される何か内容", time.Now()) // 申請を提出
+	assert.Nil(t, err)
+	assert.Equal(t, true, app.IsApplying())  // 申請中である
+	assert.Equal(t, false, app.IsApproved()) // 申請中であるので承認されていない
+}
+
+func TestApplication_Approve(t *testing.T) {
+	app := submit() // 申請を作成し提出
+
+	err := app.Approve("たなか　れびゅあ", time.Now()) // 承認
+	assert.Nil(t, err)
+	assert.Equal(t, false, app.IsApplying()) // 承認されたので申請中でない
+	assert.Equal(t, true, app.IsApproved())  // 承認されている
+}
+
+func TestApplication_Approve_InvalidStatus(t *testing.T) {
+	app := NewApplication("さとう　しんせいしゃ", time.Now()) // 申請を作成
+
+	err := app.Approve("たなか　れびゅあ", time.Now())       // 申請を提出しないで承認しようとしている
+	assert.Equal(t, err, ErrInvalidStatusTransition) // 申請ステータス遷移エラー
+}
+
+func TestApplication_Reject(t *testing.T) {
+	app := submit() // 申請を作成し提出
+
+	err := app.Reject("たなか　れびゅあ", "申請内容に誤字があったため", time.Now()) // 否認
+	assert.Nil(t, err)
+	assert.Equal(t, false, app.IsApplying()) // 否認されたので申請中でない
+	assert.Equal(t, true, app.IsRejected())  // 否認されている
+}
+
+func TestApplication_Reject_InvalidStatus(t *testing.T) {
+	app := NewApplication("さとう　しんせいしゃ", time.Now()) // 申請を作成
+
+	err := app.Reject("たなか　れびゅあ", "申請内容に誤字があったため", time.Now()) // 否認       // 申請を提出しないで承認しようとしている
+	assert.Equal(t, err, ErrInvalidStatusTransition)           // 申請ステータス遷移エラー
+}
+
+func TestApplication_Resubmit(t *testing.T) {
+	app := submit() // 申請を作成し提出
+	_ = app.Reject("たなか　れびゅあ", "申請内容に誤字があったため", time.Now())
+
+	err := app.Resubmit("再提出した何か内容")
+	assert.Nil(t, err)
+	assert.Equal(t, true, app.IsApplying())  // 再提出したので申請中である
+	assert.Equal(t, false, app.IsRejected()) // 再提出したので否認とならない
+}
