ec53707b5d492bcbccaba227932253e20cbd252b
kumackey <kumak1t09e0@gmail.com>
2022-01-23 18:36:50 +0900

    第4章: Cookieを用いたフラッシュメッセージ

--- programming_practice/ch04/main.go
+++ programming_practice/ch04/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/base64"
+	json2 "encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -13,7 +16,102 @@ func main() {
 	http.HandleFunc("/headers", headers)
 	http.HandleFunc("/body", body)
 	http.HandleFunc("/process", process)
+	http.HandleFunc("/write", writeExample)
+	http.HandleFunc("/writeheader", writeHeaderExample)
+	http.HandleFunc("/redirect", headerExample)
+	http.HandleFunc("/json", jsonExample)
+	http.HandleFunc("/set_cookie", setCookie)
+	http.HandleFunc("/get_cookie", getCookie)
+	http.HandleFunc("/set_message", setMessage)
+	http.HandleFunc("/get_message", getMessage)
+
 	server.ListenAndServe()
+}
+
+func getMessage(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("flash")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			fmt.Fprintln(w, "メッセージがありません")
+		}
+	} else {
+		rc := http.Cookie{
+			Name:    "flash",
+			MaxAge:  -1,
+			Expires: time.Unix(1, 0),
+		}
+		http.SetCookie(w, &rc)
+		val, _ := base64.URLEncoding.DecodeString(c.Value)
+		fmt.Fprintln(w, string(val))
+	}
+}
+
+func setMessage(w http.ResponseWriter, r *http.Request) {
+	msg := []byte("Hello World")
+	c := http.Cookie{
+		Name:  "flash",
+		Value: base64.URLEncoding.EncodeToString(msg),
+	}
+	http.SetCookie(w, &c)
+}
+
+func getCookie(w http.ResponseWriter, r *http.Request) {
+	c1, err := r.Cookie("first_cookie")
+	if err != nil {
+		fmt.Fprintln(w, "Cannot get the first cookie")
+	}
+
+	cs := r.Cookies()
+	fmt.Fprintln(w, c1)
+	fmt.Fprintln(w, cs)
+}
+
+func setCookie(w http.ResponseWriter, r *http.Request) {
+	c1 := http.Cookie{
+		Name:     "first_cookie",
+		Value:    "Go Web Programming",
+		HttpOnly: true,
+	}
+
+	c2 := http.Cookie{
+		Name:     "second_cookie",
+		Value:    "Manning Publications Co",
+		HttpOnly: true,
+	}
+
+	http.SetCookie(w, &c1)
+	http.SetCookie(w, &c2)
+}
+
+type Post struct {
+	User    string
+	Threads []string
+}
+
+func jsonExample(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	post := &Post{
+		User:    "Sau Sheong",
+		Threads: []string{"1番目", "2番目", "3番目"},
+	}
+	json, _ := json2.Marshal(post)
+	w.Write(json)
+}
+
+func headerExample(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Location", "http://google.com")
+	w.WriteHeader(302)
+}
+
+func writeHeaderExample(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(501)
+	fmt.Fprintln(w, "そのようなサービスはありません")
+}
+
+func writeExample(w http.ResponseWriter, r *http.Request) {
+	str := `<html>
+<head><title>Go Web Programming</title></head><body><h1>Hello</h1></body></html`
+	w.Write([]byte(str))
 }
 
 func process(w http.ResponseWriter, r *http.Request) {
