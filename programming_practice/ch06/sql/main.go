c0f6bdbba2ac46b6a7b6c882591f3f87667cf12a
kumackey <kumak1t09e0@gmail.com>
2022-02-23 18:12:40 +0900

    1対多の関係

--- programming_practice/ch06/sql/main.go
+++ programming_practice/ch06/sql/main.go
@@ -16,47 +16,20 @@ func init() {
 	}
 }
 
-type Post struct {
-	Id      int
-	Content string
-	Author  string
-}
-
-func (post *Post) Create() {
-	statement := "insert into posts (content, author) values ($1, $2) returning id"
-	stmt, err := Db.Prepare(statement)
-	if err != nil {
-		return
-	}
-	defer stmt.Close()
-	err = stmt.QueryRow(post.Content, post.Author).Scan(&post.Id)
-	return
-}
-
-func (post *Post) Update() (err error) {
-	_, err = Db.Exec("update posts set content = $2, author = $3 where id = $1", post.Id, post.Content, post.Author)
-	return
-}
-
-func (post *Post) Delete() (err error) {
-	_, err = Db.Exec("delete from posts where id = $1", post.Id)
-	return
-}
-
 func main() {
 	post := Post{Content: "Hello", Author: "kumaki"}
-	fmt.Println(post)
 	post.Create()
+
+	comment := Comment{Content: "良い投稿だね！", Author: "Joe", Post: &post}
+	comment.Create()
+	comment2 := Comment{Content: "良い投稿だね！2", Author: "Joe", Post: &post}
+	comment2.Create()
 
 	readPost, _ := GetPost(post.Id)
 	fmt.Println(readPost)
 
-	readPost.Content = "Bonjour Monde!"
-	readPost.Author = "Pierre"
-	readPost.Update()
-
-	posts, _ := Posts(10)
-	fmt.Println(posts)
+	//posts, _ := Posts(10)
+	//fmt.Println(posts)
 
 	readPost.Delete()
 }
@@ -81,6 +54,22 @@ func Posts(limit int) (posts []Post, err error) {
 
 func GetPost(id int) (post Post, err error) {
 	post = Post{}
+	post.Comments = []Comment{}
 	err = Db.QueryRow("select id, content, author from posts where id = $1", id).Scan(&post.Id, &post.Content, &post.Author)
+
+	rows, err := Db.Query("select id, content, author from comments where post_id = $1", id)
+	if err != nil {
+		return
+	}
+
+	for rows.Next() {
+		comment := Comment{Post: &post}
+		err = rows.Scan(&comment.Id, &comment.Content, &comment.Author)
+		if err != nil {
+			return
+		}
+		post.Comments = append(post.Comments, comment)
+	}
+	rows.Close()
 	return
 }
---
+++ programming_practice/ch06/sql/post.go
@@ -0,0 +1,29 @@
+package main
+
+type Post struct {
+	Id       int
+	Content  string
+	Author   string
+	Comments []Comment
+}
+
+func (post *Post) Create() {
+	statement := "insert into posts (content, author) values ($1, $2) returning id"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	err = stmt.QueryRow(post.Content, post.Author).Scan(&post.Id)
+	return
+}
+
+func (post *Post) Update() (err error) {
+	_, err = Db.Exec("update posts set content = $2, author = $3 where id = $1", post.Id, post.Content, post.Author)
+	return
+}
+
+func (post *Post) Delete() (err error) {
+	_, err = Db.Exec("delete from posts where id = $1", post.Id)
+	return
+}
