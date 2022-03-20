778266b263373e408b6ff0f36eab54b561ac42eb
kumackey <kumak1t09e0@gmail.com>
2022-03-20 14:09:25 +0900

    mock化

--- programming_practice/ch07/web/main_test.go
+++ programming_practice/ch07/web/main_test.go
@@ -1,22 +1,51 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
+type FakePost struct {
+	Id      int
+	Content string
+	Author  string
+}
+
+func (f *FakePost) fetch(id int) error {
+	f.Id = id
+	return nil
+}
+
+func (f *FakePost) create() error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (f *FakePost) update() error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (f *FakePost) delete() error {
+	//TODO implement me
+	panic("implement me")
+}
+
 func TestHanleGet(t *testing.T) {
-	//mux := http.NewServeMux()
-	//mux.HandleFunc("/post/", handleRequest())
-	//writer := httptest.NewRecorder()
-	//request, _ := http.NewRequest("GET", "/post/1", nil)
-	//mux.ServeHTTP(writer, request)
-	//
-	//if writer.Code != 200 {
-	//	t.Errorf("Response code is %v", writer.Code)
-	//}
-	//var post Post
-	//json.Unmarshal(writer.Body.Bytes(), &post)
-	//if post.Id != 1 {
-	//	t.Error("Cannnot fetch JSON POST")
-	//}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/post/", handleRequest(&FakePost{}))
+	writer := httptest.NewRecorder()
+	request, _ := http.NewRequest("GET", "/post/1", nil)
+	mux.ServeHTTP(writer, request)
+
+	if writer.Code != 200 {
+		t.Errorf("Response code is %v", writer.Code)
+	}
+	var post Post
+	json.Unmarshal(writer.Body.Bytes(), &post)
+	if post.Id != 1 {
+		t.Error("Cannnot fetch JSON POST")
+	}
 }
