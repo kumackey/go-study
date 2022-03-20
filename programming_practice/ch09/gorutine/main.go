649e5443e383c56ca641ddc16f6eeed03e9b2854
kumackey <kumak1t09e0@gmail.com>
2022-03-20 22:09:59 +0900

    ベンチマーク比較

--- programming_practice/ch09/gorutine/main.go
+++ programming_practice/ch09/gorutine/main.go
@@ -14,20 +14,20 @@ func printNumbers1() {
 func printNumbers2() {
 	for i := 0; i < 10; i++ {
 		time.Sleep(1 * time.Microsecond)
-		fmt.Printf("%d", i)
+		fmt.Printf("%d ", i)
 	}
 }
 
 func printLetters1() {
 	for i := 'A'; i < 'A'+10; i++ {
-		fmt.Printf("%c", i)
+		fmt.Printf("%c ", i)
 	}
 }
 
 func printLetters2() {
 	for i := 'A'; i < 'A'+10; i++ {
 		time.Sleep(1 * time.Microsecond)
-		fmt.Printf("%c", i)
+		fmt.Printf("%c ", i)
 	}
 }
 
