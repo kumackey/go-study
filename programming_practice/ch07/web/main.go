8759e47f61ea315fb698126972eadf8298cc3ebb
kumackey <kumak1t09e0@gmail.com>
2022-03-20 13:59:10 +0900

    interface化

--- programming_practice/ch07/web/main.go
+++ programming_practice/ch07/web/main.go
@@ -9,107 +9,112 @@ import (
 	"strconv"
 )
 
-var Db *sql.DB
-
-func init() {
+func main() {
 	var err error
-	Db, err = sql.Open("postgres", "user=gwp dbname=gwp password=gwp sslmode=disable")
+	db, err := sql.Open("postgres", "user=gwp dbname=gwp password=gwp sslmode=disable")
 	if err != nil {
 		panic(err)
 	}
-}
 
-func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
-	http.HandleFunc("/post/", handleRequest)
+	http.HandleFunc("/post/", handleRequest(&Post{Db: db}))
 	server.ListenAndServe()
 }
 
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-	var err error
-	switch r.Method {
-	case "GET":
-		err = handleGet(w, r)
-	case "POST":
-		err = handlePost(w, r)
-	case "PUT":
-		err = handlePut(w, r)
-	case "DELETE":
-		err = handleDelete(w, r)
+type Text interface {
+	fetch(id int) error
+	create() error
+	update() error
+	delete() error
+}
 
-	}
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+func handleRequest(t Text) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var err error
+		switch r.Method {
+		case "GET":
+			err = handleGet(w, r, t)
+		case "POST":
+			err = handlePost(w, r, t)
+		case "PUT":
+			err = handlePut(w, r, t)
+		case "DELETE":
+			err = handleDelete(w, r, t)
+
+		}
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
 }
 
-func handleDelete(w http.ResponseWriter, r *http.Request) error {
+func handleDelete(w http.ResponseWriter, r *http.Request, t Text) error {
+	//id, err := strconv.Atoi(path.Base(r.URL.Path))
+	//if err != nil {
+	//	return err
+	//}
+	//post, err := fetch(id)
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//err = post.delete()
+	//if err != nil {
+	//	return err
+	//}
+	//w.WriteHeader(200)
+	return nil
+}
+
+func handlePut(w http.ResponseWriter, r *http.Request, t Text) error {
+	//id, err := strconv.Atoi(path.Base(r.URL.Path))
+	//if err != nil {
+	//	return err
+	//}
+	//err = t.fetch(id)
+	//if err != nil {
+	//	return err
+	//}
+	//len := r.ContentLength
+	//body := make([]byte, len)
+	//r.Body.Read(body)
+	//json.Unmarshal(body, &post)
+	//err = t.update()
+	//if err != nil {
+	//	return err
+	//}
+	//w.WriteHeader(200)
+	return nil
+}
+
+func handlePost(w http.ResponseWriter, r *http.Request, t Text) error {
+	//len := r.ContentLength
+	//body := make([]byte, len)
+	//r.Body.Read(body)
+	//var post Post
+	//json.Unmarshal(body, &post)
+	//post.create()
+	//w.WriteHeader(200)
+	//id := strconv.Itoa(post.Id)
+	//w.Write([]byte(id))
+	return nil
+}
+
+func handleGet(w http.ResponseWriter, r *http.Request, t Text) error {
 	id, err := strconv.Atoi(path.Base(r.URL.Path))
 	if err != nil {
 		return err
 	}
-	post, err := retrieve(id)
+
+	err = t.fetch(id)
 	if err != nil {
 		return err
 	}
 
-	err = post.delete()
-	if err != nil {
-		return err
-	}
-	w.WriteHeader(200)
-	return nil
-}
-
-func handlePut(w http.ResponseWriter, r *http.Request) error {
-	id, err := strconv.Atoi(path.Base(r.URL.Path))
-	if err != nil {
-		return err
-	}
-	post, err := retrieve(id)
-	if err != nil {
-		return err
-	}
-	len := r.ContentLength
-	body := make([]byte, len)
-	r.Body.Read(body)
-	json.Unmarshal(body, &post)
-	err = post.update()
-	if err != nil {
-		return err
-	}
-	w.WriteHeader(200)
-	return nil
-}
-
-func handlePost(w http.ResponseWriter, r *http.Request) error {
-	len := r.ContentLength
-	body := make([]byte, len)
-	r.Body.Read(body)
-	var post Post
-	json.Unmarshal(body, &post)
-	post.create()
-	w.WriteHeader(200)
-	id := strconv.Itoa(post.Id)
-	w.Write([]byte(id))
-	return nil
-}
-
-func handleGet(w http.ResponseWriter, r *http.Request) error {
-	id, err := strconv.Atoi(path.Base(r.URL.Path))
-	if err != nil {
-		return err
-	}
-
-	post, err := retrieve(id)
-	if err != nil {
-		return err
-	}
-
-	output, err := json.MarshalIndent(&post, "", "\t\t")
+	output, err := json.MarshalIndent(t, "", "\t\t")
 	if err != nil {
 		return err
 	}
--- programming_practice/ch07/web/post.go
+++ programming_practice/ch07/web/post.go
@@ -1,35 +1,36 @@
 package main
 
+import "database/sql"
+
 type Post struct {
-	Id       int
-	Content  string
-	Author   string
-	Comments []Comment
+	Db      *sql.DB
+	Id      int
+	Content string
+	Author  string
 }
 
-func (post *Post) create() {
+func (post *Post) create() error {
 	statement := "insert into posts (content, author) values ($1, $2) returning id"
-	stmt, err := Db.Prepare(statement)
+	stmt, err := post.Db.Prepare(statement)
 	if err != nil {
-		return
+		return err
 	}
 	defer stmt.Close()
 	err = stmt.QueryRow(post.Content, post.Author).Scan(&post.Id)
-	return
+	return err
 }
 
-func (post *Post) update() (err error) {
-	_, err = Db.Exec("update posts set content = $2, author = $3 where id = $1", post.Id, post.Content, post.Author)
-	return
+func (post *Post) update() error {
+	_, err := post.Db.Exec("update posts set content = $2, author = $3 where id = $1", post.Id, post.Content, post.Author)
+	return err
 }
 
-func (post *Post) delete() (err error) {
-	_, err = Db.Exec("delete from posts where id = $1", post.Id)
-	return
+func (post *Post) delete() error {
+	_, err := post.Db.Exec("delete from posts where id = $1", post.Id)
+	return err
 }
 
-func retrieve(id int) (post Post, err error) {
-	post = Post{}
-	err = Db.QueryRow("select id, content, author from posts where id = $1", id).Scan(&post.Id, &post.Content, &post.Author)
-	return
+func (post *Post) fetch(id int) error {
+	err := post.Db.QueryRow("select id, content, author from posts where id = $1", id).Scan(&post.Id, &post.Content, &post.Author)
+	return err
 }
