d17481762d7ff2b5d4a812ee29c19d6fe19d8ef0
kumackey <kumak1t09e0@gmail.com>
2022-04-09 14:51:42 +0900

    waitGroup

--- programming_practice/ch09/gorutine/main.go
+++ programming_practice/ch09/gorutine/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -11,24 +12,18 @@ func printNumbers1() {
 	}
 }
 
-func printNumbers2() {
-	for i := 0; i < 10; i++ {
-		time.Sleep(1 * time.Microsecond)
-		fmt.Printf("%d ", i)
-	}
-}
-
 func printLetters1() {
 	for i := 'A'; i < 'A'+10; i++ {
 		fmt.Printf("%c ", i)
 	}
 }
 
-func printLetters2() {
+func printLetters2(wg *sync.WaitGroup) {
 	for i := 'A'; i < 'A'+10; i++ {
 		time.Sleep(1 * time.Microsecond)
 		fmt.Printf("%c ", i)
 	}
+	wg.Done()
 }
 
 func print1() {
@@ -41,11 +36,18 @@ func goPrint1() {
 	go printLetters1()
 }
 
-func goPrint2() {
-	go printNumbers2()
-	go printLetters2()
+func printNumbers2(wg *sync.WaitGroup) {
+	for i := 0; i < 10; i++ {
+		time.Sleep(1 * time.Microsecond)
+		fmt.Printf("%d ", i)
+	}
+	wg.Done()
 }
 
 func main() {
-
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go printNumbers2(&wg)
+	go printLetters2(&wg)
+	wg.Wait()
 }
