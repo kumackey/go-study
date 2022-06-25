d1d466cc094755188bcf0d563f0e217fbf3fdc87
kumackey <kumak1t09e0@gmail.com>
2022-06-25 22:48:06 +0900

    pingの結果をより適切にハンドリング

--- shell/ch09/CheckHostname.sh
+++ shell/ch09/CheckHostname.sh
@@ -5,17 +5,14 @@ CheckHostname() {
   _HOST=${1:-$(hostname)}
 
   case $(uname -s) in
-  FreeBSD) _PING="ping -c1 $_HOST" ;;
-  Linux) _PING="ping -c1 $_HOST" ;;
-  OSF1) _PING="ping -c1 $_HOST" ;;
+  FreeBSD | Linux | OSF1 | Darwin) _PING="ping -c1 $_HOST" ;;
   HP-UX) _PING="ping $_HOST 64 1" ;;
-  Darwin) _PING="ping -c1 $_HOST" ;;
   *) return 1 ;;
   esac
 
-  if [ $($_PING 2>&1 | grep -ci "Unknown host") -eq 0 ]; then
-    return 1
-  else
-    return 0
-  fi
+  $_PING >/dev/null 2>&1
+  return $?
 }
+
+#CheckHostname
+#echo $?
