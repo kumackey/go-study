7b9a583b0e44d8910bf84ec9d04ede256ae79cbc
kumackey <kumak1t09e0@gmail.com>
2022-10-11 21:41:23 +0900

    grpcサーバーを動かす

---
+++ go/grpc/cmd/server/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	hellopb "example.com/go-mod-test/grpc/pkg/grpc"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	port := 8080
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		panic(err)
+	}
+
+	s := grpc.NewServer()
+
+	hellopb.RegisterGreetingServiceServer(s, NewMyServer())
+
+	reflection.Register(s)
+
+	go func() {
+		log.Printf("start gRPC server port: %v", port)
+		err := s.Serve(listener)
+		if err != nil {
+			return
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("stopping gRPC server...")
+	s.GracefulStop()
+}
+
+func NewMyServer() *myServer {
+	return &myServer{}
+}
+
+type myServer struct {
+	hellopb.UnsafeGreetingServiceServer
+}
+
+func (s *myServer) Hello(ctx context.Context, req *hellopb.HelloRequest) (*hellopb.HelloResponse, error) {
+	return &hellopb.HelloResponse{
+		Message: fmt.Sprintf("Hello, %s", req.GetName()),
+	}, nil
+}
