94971ab2a5ae63d67bf3f8b0e19aa1ce44582032
kumackey <kumak1t09e0@gmail.com>
2023-06-03 20:08:16 +0900

    todo-webapp

--- go/mtn/cli/cmd/todo-webapp/main.go
+++ go/mtn/cli/cmd/todo-webapp/main.go
@@ -26,6 +26,51 @@ var static embed.FS
 //go:embed templates
 var templates embed.FS
 
+type Todo struct {
+	bun.BaseModel `bun:"table:todos,alias:t"`
+	ID            int64     `bun:"id,pk,autoincrement"`
+	Content       string    `bun:"content,notnull"`
+	Done          bool      `bun:"done"`
+	Until         time.Time `bun:"until,nullzero"`
+	CreatedAt     time.Time
+	UpdatedAt     time.Time `bun:",nullzero"`
+	DeletedAt     time.Time `bun:",soft_delete,nullzero"`
+}
+
+type Data struct {
+	Errors []error
+	Todos  []Todo
+}
+
+func customFunc(todo *Todo) func([]string) []error {
+	return func(values []string) []error {
+		if len(values) == 0 || values[0] == "" {
+			return nil
+		}
+		dt, err := time.Parse("2006-01-02T15:04 MST", values[0]+" JST")
+		if err != nil {
+			return []error{echo.NewBindingError("until", values[0:1], "failed to parse time", err)}
+		}
+		todo.Until = dt
+		return nil
+	}
+}
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func formatDateTime(d time.Time) string {
+	if d.IsZero() {
+		return ""
+	}
+	return d.Format("2006-01-02 15:04")
+}
+
 func main() {
 	sqldb, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
@@ -123,49 +168,4 @@ func main() {
 	fileServer := http.FileServer(http.FileSystem(http.FS(staticFs)))
 	e.GET("/static/*", echo.WrapHandler(http.StripPrefix("/static/", fileServer)))
 	e.Logger.Fatal(e.Start(":8989"))
-}
-
-func customFunc(todo *Todo) func([]string) []error {
-	return func(values []string) []error {
-		if len(values) == 0 || values[0] == "" {
-			return nil
-		}
-		dt, err := time.Parse("2006-01-02T15:04 MST", values[0]+" JST")
-		if err != nil {
-			return []error{echo.NewBindingError("until", values[0:1], "failed to parse time", err)}
-		}
-		todo.Until = dt
-		return nil
-	}
-}
-
-type Todo struct {
-	bun.BaseModel `bun:"table:todos,alias:t"`
-	ID            int64     `bun:"id,pk,autoincrement"`
-	Content       string    `bun:"content,notnull"`
-	Done          bool      `bun:"done"`
-	Until         time.Time `bun:"until,nullzero"`
-	CreatedAt     time.Time
-	UpdatedAt     time.Time `bun:",nullzero"`
-	DeletedAt     time.Time `bun:",soft_delete,nullzero"`
-}
-
-type Data struct {
-	Errors []error
-	Todos  []Todo
-}
-
-type Template struct {
-	templates *template.Template
-}
-
-func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
-
-func formatDateTime(d time.Time) string {
-	if d.IsZero() {
-		return ""
-	}
-	return d.Format("2006-01-02 15:04")
 }
