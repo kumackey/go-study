42f0947ec9cf68fceede6c09206e73db3ca60928
kumackey <kumak1t09e0@gmail.com>
2023-07-15 12:15:29 +0900

    オーバーフローの実験

--- go/assemble/main.go
+++ go/assemble/main.go
@@ -1,19 +1,38 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
-	var a uint64 = math.MaxUint64
-	println(a) //Output: 18446744073709551615
+	fls := make(map[int]float32)
+	for i := 0; i < 100000; i++ {
+		fls[i] = rand.Float32()
+	}
 
-	var b = a + 1
-	println(b) //Output: 0
+	sum := func(fls map[int]float32) float32 {
+		var sum float32
+		for _, f := range fls {
+			// goのmapのループは順番がランダム
+			sum += f
+		}
+		return sum
+	}
+
+	// 同じfloat32のmapに対してsumを取ってるので、同じになるはず・・・？
+	fmt.Println(sum(fls))
+	fmt.Println(sum(fls))
+	fmt.Println(sum(fls))
+	fmt.Println(sum(fls))
+	fmt.Println(sum(fls))
+
+	// Output:
+	// 49923.273
+	// 49922.965
+	// 49923.14
+	// 49923.47
+	// 49922.89
+
+	// ならないですね〜😇
 }
-
-//func main() {
-//	a := math.MaxInt64
-//	println(a) //Output: 9223372036854775807
-//
-//	b := a + 1
-//	println(b) //Output: -9223372036854775808
-//}
