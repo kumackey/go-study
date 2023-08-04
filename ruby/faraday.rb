3240907da3fdc497a484603c45fa5c33d075479a
kumackey <kumak1t09e0@gmail.com>
2023-08-05 00:08:40 +0900

    add: faraday

--- ruby/faraday.rb
+++ ruby/faraday.rb
@@ -14,8 +14,8 @@ Faraday.delete('http://localhost:8000', { name: 'Alice' })
 # (byebug) exclusive_url
 # #<URI::HTTP http://localhost:8000?name=Alice>
 
-Faraday.delete('http://localhost:8000', { name: 'Alice' }) do |req|
+Faraday.delete('http://localhost:8000') do |req|
   req.body = { name: 'Alice' }.to_json
 end
 # (byebug) request
-# #<struct Faraday::Request http_method=:delete, path="http://localhost:8000", params={"name"=>"Alice"}, headers={"User-Agent"=>"Faraday v2.7.10"}, body="{\"name\":\"Alice\"}", options=#<Faraday::RequestOptions (empty)>>
+# #<struct Faraday::Request http_method=:delete, path="http://localhost:8000", params={}, headers={"User-Agent"=>"Faraday v2.7.10"}, body="{\"name\":\"Alice\"}", options=#<Faraday::RequestOptions (empty)>>
